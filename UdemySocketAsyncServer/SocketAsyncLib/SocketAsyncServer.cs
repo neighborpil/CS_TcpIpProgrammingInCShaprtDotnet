0770b78a0dd92d3e8c7414ee60665ba94416978e
Neighborpil <neighborpil@naver.com>
2020-04-14 07:40:08 +0900

    lec 21 making

---
+++ UdemySocketAsyncServer/SocketAsyncLib/SocketAsyncServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketAsyncLib
+{
+    public class SocketAsyncServer
+    {
+        private IPAddress _ipAddress;
+
+        private int _port;
+
+        public void StartListeningForIncomingConnection(IPAddress ipAddress = null, int port = 50001)
+        {
+
+        }
+    }
+}
