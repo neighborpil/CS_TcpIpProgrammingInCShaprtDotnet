19dcd94607f3ab43f2c53a343a72734934f8af7b
Neighborpil <neighborpil@naver.com>
2020-06-19 09:25:45 +0900

    tcp async client start

---
+++ UdemySocketAsyncServer/SocketAsyncLib/SocketAsyncClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace SocketAsyncLib
+{
+    public class SocketAsyncClient
+    {
+        private IPAddress _serverIpAddress;
+
+        private int _serverPort;
+
+        private TcpClient _client;
+
+        public SocketAsyncClient()
+        {
+            _serverIpAddress = null;
+            _client = null;
+            _serverPort = -1;
+        }
+
+        public IPAddress ServerIpAddress
+        {
+            get => _serverIpAddress;
+        }
+
+        public int ServerPort
+        {
+            get => _serverPort;
+        }
+
+        public bool SetServerIpAddress(string ipAddressServer)
+        {
+            IPAddress ipAddress = null;
+
+            if (!IPAddress.TryParse(ipAddressServer, out ipAddress))
+            {
+                Console.WriteLine("Invalid server IP supplied.");
+                return false;
+            }
+
+            _serverIpAddress = ipAddress;
+
+            return true;
+        }
+
+        public bool SetPortNumber(string serverPort)
+        {
+            int portNumber = 0;
+            if (!int.TryParse(serverPort, out portNumber))
+            {
+                Console.WriteLine("Invalid server port supplied.");
+                return false;
+            }
+
+            if (portNumber <= 0 || portNumber > 65535)
+            {
+                Console.WriteLine("Port number must be between 0 and 65535.");
+                return false;
+            }
+
+            _serverPort = portNumber;
+
+            return true;
+        }
+
+        public async Task ConnectToServer()
+        {
+            if (_client == null)
+            {
+                _client = new TcpClient();
+            }
+
+            try
+            {
+                await _client.ConnectAsync(_serverIpAddress, _serverPort);
+                Console.WriteLine($"Connected to server IP/Port: {_serverIpAddress} / {_serverPort}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ UdemySocketAsyncServer/UdemySocketAsyncClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using SocketAsyncLib;
+
+namespace UdemySocketAsyncClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SocketAsyncClient();
+
+            Console.WriteLine("***Socket Async Client example***");
+            Console.WriteLine("Please type a valid server IP address and press enter:");
+
+            string strIpAddress = Console.ReadLine();
+
+            Console.WriteLine("Please supply a valid port number 0 - 65535 and press enter:");
+            string strPortInput = Console.ReadLine();
+
+            if (!client.SetServerIpAddress(strIpAddress) || !client.SetPortNumber(strPortInput))
+            {
+                Console.WriteLine($"Wrong IP adress or port number supplied - {strIpAddress} / {strPortInput}");
+                Console.ReadKey();
+                return;
+            }
+
+            client.ConnectToServer();
+
+            string strInputUser = null;
+
+            do
+            {
+                strInputUser = Console.ReadLine();
+            } while (strInputUser != "<EXIT>");
+        }
+    }
+}
